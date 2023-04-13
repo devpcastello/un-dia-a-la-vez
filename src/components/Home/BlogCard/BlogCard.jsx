c1e3012e6ce2fbc558455b4ee16b122423971aa4
enrique palomino <octepc.97@gmail.com>
2023-04-13 18:14:42 -0500

    feat: :sparkles: Worked on the blog section. Added the data and dynamic render

--- src/components/Home/BlogCard/BlogCard.jsx
+++ src/components/Home/BlogCard/BlogCard.jsx
@@ -1,31 +1,33 @@
 import React from "react";
 
-import imgStock from "../../../assets/img-stock-1.png";
+import { posts } from "../../../data/posts";
 
 export const BlogCard = () => {
   return (
-    <div className="bg-gray-2 w-11/12 h-96 rounded-lg mx-auto  text-center">
-      <div className="h-48 w-full ">
-        <img
-          className="card-image w-full h-full rounded-t-lg"
-          src={imgStock}
-          alt=""
-        />
-      </div>
-      <div className="p-5">
-        <p className="mb-3">01/01/2023</p>
-        <h3 className="text-2xl mb-3">
-          Primer post que se está publicando en el blog
-        </h3>
-        <div className=" w-full h-12 overflow-hidden text-clip">
-          <p className="text-clip overflow-hidden">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis
-            quia ad tempora vitae. Saepe deserunt dolore iure fuga libero quod
-            nostrum dignissimos illo voluptas pariatur, rem aliquid repudiandae
-            inventore debitis.
-          </p>
+    <div className="flex flex-col gap-8">
+      {posts.map(({ title, image, date, description }) => (
+        <div
+          key={title}
+          className="bg-gray-2 w-11/12 h-96 rounded-lg mx-auto  text-center"
+        >
+          <div className="h-48 w-full ">
+            <img
+              className="card-image w-full h-full rounded-t-lg"
+              src={image}
+              alt=""
+            />
+          </div>
+          <div className="p-5">
+            <p className="mb-3 text-gray-6 font-light">{date}</p>
+            <h3 className="text-2xl mb-3 text-gray-10 font-medium">{title}</h3>
+            <div className=" w-full h-12 overflow-hidden text-clip">
+              <p className="text-clip overflow-hidden text-gray-8 font-normal">
+                {description}
+              </p>
+            </div>
+          </div>
         </div>
-      </div>
+      ))}
     </div>
   );
 };
--- src/components/Home/BlogSection/BlogSection.jsx
+++ src/components/Home/BlogSection/BlogSection.jsx
@@ -3,14 +3,17 @@ import { BlogCard } from "../BlogCard/BlogCard";
 
 export const BlogSection = () => {
   return (
-    <div className="h-screen bg-dark-green">
+    <div className="h-[1400px] bg-dark-green overflow-hidden flex flex-col items-center justify-start gap-8">
       <div>
-        <h2 className="ml-3 text-xl text-white mb-5">Descubre mi blog:</h2>
-        <button className="bright-button py-2 px-4 rounded-full mb-5 text-center">
-          Descubrir más
-        </button>
+        <h2 className="ml-3 text-4xl text-center text-white pt-10 pb-4">
+          Descubre mi blog:
+        </h2>
       </div>
       <BlogCard />
+
+      <button className="bright-button py-2 px-4 rounded-full mb-5 text-center">
+        Descubrir más
+      </button>
     </div>
   );
 };
