f44f49fe39ff6984b73163708a706ae8b6d24842
enrique palomino <octepc.97@gmail.com>
2023-04-13 18:55:35 -0500

    build: Commit pre-building

--- src/components/Home/BlogCard/BlogCard.jsx
+++ src/components/Home/BlogCard/BlogCard.jsx
@@ -1,19 +1,3 @@
-import { BlogCard } from "../../Home/BlogCard/BlogCard";
-
-export const Blog = () => {
-  return (
-    <section className="h-auto w-full py-14">
-      <div className=" bg-dark-green h-fit w-full flex gap-8 flex-col">
-        <h2 className="ml-3 text-4xl text-center text-white pt-10 pb-4">
-          Descubre mi blog:
-        </h2>
-        <BlogCard />
-      </div>
-    </section>
-  );
-};
-import React from "react";
-
 import { posts } from "../../../data/posts";
 
 export const BlogCard = () => {
--- src/components/Home/Subscription/Subscription.jsx
+++ src/components/Home/Subscription/Subscription.jsx
@@ -4,31 +4,33 @@ import dulcinea from "../../../assets/dulcinea-3.png";
 
 export const Subscription = () => {
   return (
-    <div className="bg-light-green h-screen w-full flex flex-col justify-center items-center">
-      <div className="w-9/12 h-auto">
+    <div className="bg-light-green h-[700px] w-full flex flex-col justify-center items-center">
+      <div className="w-10/12 h-auto">
         <div className="text-center mb-5">
-          <h3 className="text-xl mb-3">Subscríbete a nuestro News Letter!</h3>
-          <p>
+          <h3 className="text-3xl my-8 font-medium text-gray-10">
+            Subscríbete a nuestro News Letter!
+          </h3>
+          <p className="text-gray-8 font-light text-lg">
             Recibe contenido exclusivo en tu correo personal de forma semanal:
             libros, podcast, videos, conferencias, webinars entre otros.
           </p>
         </div>
-        <div>
+        <div className="flex flex-col justify-center gap-2">
           <label className="">Nombres:</label>
           <input
-            className="rounded-full mt-1 mb-2 px-1 py-2 bg-bright-green"
+            className="rounded-full px-4 py-2 w-full bg-bright-green text-white"
             type="text"
             name="name"
             id="name"
           />
           <label className="">Email:</label>
           <input
-            className="rounded-full mt-1 mb-2 px-1 py-2 bg-bright-green"
+            className="rounded-full px-4 py-2 w-full bg-bright-green text-start text-white"
             type="email"
             name="email"
             id="email"
           />
-          <button className="dark-button rounded-full py-2 px-4 mt-2">
+          <button className="bg-dark-green text-white rounded-full py-2 px-4 mt-4 w-full">
             Subscribirme!
           </button>
         </div>
