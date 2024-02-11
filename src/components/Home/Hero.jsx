16ec2dbc8f88af1048f8d4d63598d00a59037dd4
Enrique Palomino <octepc.97@gmail.com>
2024-02-11 12:31:00 -0500

    UPDATE some styles

--- src/components/Home/Hero.jsx
+++ src/components/Home/Hero.jsx
@@ -1,15 +1,18 @@
 import dulcinea from "../../assets/dulcinea-1.png";
 import blob from "../../assets/blob.svg";
 import { Link } from "react-router-dom";
+import bgImage from "../../assets/home-background-image.jpg";
 
 export const Hero = () => {
   return (
     <div
-      className=" h-screen w-full grid grid-rows-2 justify-center items-center md:grid-cols-2  gap-8"
-      id="home"
-      style={{ justifyItems: "center", alignItems: "center" }}
+      className="h-screen w-full grid grid-rows-2 justify-center items-center md:grid-cols-2 gap-8 bg-center relative"
+      style={{
+        backgroundImage: `url(${bgImage})`,
+      }}
     >
-      <div className="xl:mt-[500px]">
+      <div className="bg-gray-8 bg-opacity-20 h-full w-full absolute inset-0" />
+      <div className="xl:mt-[500px] relative">
         <div className="w-auto h-1/2 mt-[200px] flex justify-center items-center z-10 relative md:mt-[100px]">
           <img
             className="w-[400px] max-w-80 w-min-96 h-min-96 absolute z-[-10] md:min-w-[500px] md:min-h-[500px] xl:min-w-[900px] xl:min-h-[900px] animate__animated animate__fadeInUp"
@@ -23,16 +26,14 @@ export const Hero = () => {
           />
         </div>
       </div>
-      <div className=" mt-5 text-white flex flex-col justify-center items-center z-10 gap-4 md:gap-12 xl:mt-[500px]">
+      <div className="mt-5 text-white flex flex-col justify-center items-center z-20 gap-4 md:gap-12 xl:mt-[500px] relative">
         <h2 className="text-2xl font-bold text-center md:text-4xl xl:text-6xl">
           Una terapia única y enfocada en el ser humano
         </h2>
-        <p className="md:text-xl xl:text-4xl">
-          Nuestros servicios se diferencian por
-        </p>
+        {/* <p className="md:text-xl xl:text-4xl">Nuestros servicios</p> */}
       </div>
-      <button className="bg-color6 bottom-10 absolute py-2 px-4 rounded-full text-white xl:text-2xl xl:py-4 xl:px-6">
-        <Link to="/about">Quiero saber más!</Link>
+      <button className="bg-dark-green bottom-10 absolute py-2 px-4 rounded-full text-white xl:text-2xl xl:py-4 xl:px-6 z-20">
+        <Link to="/about">¿Empezamos tu proceso?</Link>
       </button>
     </div>
   );
