2170b2691b2411bb775e296aea4bae8bbe02dcd4
Enrique Palomino <octepc.97@gmail.com>
2024-02-27 11:56:11 -0500

    UPDATE the benefits component. Caminoblog is working

--- src/components/Home/Benefits.jsx
+++ src/components/Home/Benefits.jsx
@@ -1,6 +1,7 @@
 // import { benefits } from '../../data/benefits';
 import CaminoBlobMobile from '../../assets/CaminoBlobMobile';
 import CaminoBlobDesktop from '../../assets/CaminoBlobDesktop';
+import { useState, useEffect } from 'react';
 
 export const Benefits = () => {
   // const [currentImageIndex, setCurrentImageIndex] = useState(0);
@@ -44,8 +45,11 @@ export const Benefits = () => {
             </p>
           </div>
 
-          <div className='m-auto hidden justify-center lg:flex'>
+          <div className='m-auto hidden justify-center lg:flex lg:py-20'>
             <CaminoBlobDesktop width={1200} height={600} />
+          </div>
+          <div className='m-auto hidden justify-center md:flex lg:hidden lg:py-20'>
+            <CaminoBlobDesktop width={900} height={400} />
           </div>
           {/* <div className='relative hidden w-[1200px] md:flex md:min-h-[500px] md:items-center md:justify-center md:text-h6 lg:text-h5 xl:my-20'> */}
           {/* <h2 className='absolute left-0 top-0 md:text-h5 lg:left-32 lg:text-h4 xl:left-28'>
