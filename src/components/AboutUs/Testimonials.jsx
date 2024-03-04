945173217a50dd787715c09ca79e01489fbf12fc
Enrique Palomino <octepc.97@gmail.com>
2024-03-03 19:10:58 -0500

    REFACTORIZED the testimonials component

--- src/components/AboutUs/Testimonials.jsx
+++ src/components/AboutUs/Testimonials.jsx
@@ -1,23 +1,50 @@
+import { useParams } from 'react-router-dom';
 import { TestimonialCard } from './components/TestimonialCard';
+import { testimonialsData } from '../../data/aboutUsTestimonials';
+import { HeroSection } from './components/HeroSection';
 
 export const Testimonials = () => {
+  const { testimonials } = useParams();
   return (
-    <section className='flex h-auto w-full flex-col items-center justify-center'>
-      <div className='m-8'>
-        <h2 className='my-8 text-center text-4xl font-medium text-gray-10'>
-          Testimonios
-        </h2>
-        <p className='text-gray-8 xl:mx-16 xl:text-xl'>
-          ¡Aquí te presentamos algunas opiniones de nuestros clientes
-          satisfechos! Nos enorgullece saber que hemos cumplido con sus
-          expectativas y hemos brindado soluciones efectivas a sus necesidades.
-          Esperamos que sus testimonios te den la confianza que necesitas para
-          elegir nuestro producto/servicio y unirte a nuestra comunidad de
-          clientes satisfechos. ¡Gracias por confiar en nosotros!
-        </p>
-      </div>
-
-      <TestimonialCard />
-    </section>
+    <>
+      {testimonialsData.map(
+        ({
+          id,
+          heroBackground,
+          heroTitle,
+          heroMessage,
+          heroImage,
+          testimonialsTitle,
+          testimonialsDescription,
+          testimonialsCards,
+        }) => {
+          if (testimonials !== !testimonials) {
+            return (
+              <main
+                key={id}
+                className='flex h-auto w-full flex-col items-center justify-center'
+              >
+                <HeroSection
+                  heroBackground={heroBackground}
+                  heroTitle={heroTitle}
+                  heroMessage={heroMessage}
+                  heroImage={heroImage}
+                />
+                <div className='m-8'>
+                  <h2 className='my-8 text-center text-4xl font-medium text-gray-10'>
+                    {testimonialsTitle}
+                  </h2>
+                  <p className='text-gray-8 xl:mx-16 xl:text-xl'>
+                    {testimonialsDescription}
+                  </p>
+                </div>
+                <TestimonialCard cards={testimonialsCards} />
+              </main>
+            );
+          }
+          return null;
+        },
+      )}
+    </>
   );
 };
